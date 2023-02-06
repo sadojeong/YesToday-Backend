826a6eeefd2f28c4e19b28e2bdc0d2834696b397
Sado Jeong <a01063854375@gmail.com>
2023-02-06 14:34:17 +0900

    addPostResponse

--- src/main/java/dev/yestoday/yestoday/core/post/dto/PostResponse.java
+++ src/main/java/dev/yestoday/yestoday/core/post/dto/PostResponse.java
@@ -5,11 +5,14 @@ import dev.yestoday.yestoday.core.post.domain.Post;
 
 public class PostResponse {
     @JsonProperty
+    private Long id;
+    @JsonProperty
     private String content;
     @JsonProperty
     private String imageUrl;
 
     public PostResponse(Post post) {
+        this.id = post.getId();
         this.content = post.getContent();
         this.imageUrl = post.getImageUrl();
     }
